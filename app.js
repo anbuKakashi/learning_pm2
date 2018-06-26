256818c8078f562556009c35e99ff951b6a786d4
anbuKakashi <kakafuad12@gmail.com>
2018-06-26 14:56:10 +0700

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World, ini menggunakan nodejs pm2");
+  res.send("Hello World, ini menggunakan nodejs pm2-2");
 });
 
 app.listen(4000);
